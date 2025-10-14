040389398f989046bb76933878cafa74ae249a21
SylviaRemington <sylviaremington@gmail.com>
2025-10-14 10:50:32 -0700

    stories model added with title author content and ownder

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Story(models.Model):
+    def __str__(self):
+        return f'{self.title} - {self.author}'
+
+    # models.CharField is the data type and means "string"
+    title = models.CharField(max_length=80, unique=True)
+    author = models.CharField(max_length=80)
+    content = models.TextField()
+    owner = models.ForeignKey(
+        "authentication.User",
+        related_name = "stories",
+        on_delete = models.CASCADE
+    )
+    
