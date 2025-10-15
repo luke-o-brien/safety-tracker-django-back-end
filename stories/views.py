259ff4ee0985d17e6716398f8d968d6ac96f9fec
SylviaRemington <sylviaremington@gmail.com>
2025-10-15 16:11:39 -0700

    quick notes correction

--- stories/views.py
+++ stories/views.py
@@ -15,7 +15,7 @@ class StoryListView(APIView):
     def get(self, _request):
         # go to the database and get all the stories
         stories = Story.objects.all()
-        # translate the books from the database to a usable form
+        # translate the stories from the database to a usable form
         serialized_stories = StorySerializer(stories, many=True)
         # return the serialized data and a 200 status code
         return Response(serialized_stories.data, status=status.HTTP_200_OK)
