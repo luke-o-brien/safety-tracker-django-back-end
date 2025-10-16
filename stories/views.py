fddc463832440db52f9f77850892b139040987a0
SylviaRemington <sylviaremington@gmail.com>
2025-10-15 19:45:04 -0700

    creating StoryDetailView in stories views.py to get put and delete a single story

--- stories/views.py
+++ stories/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 from .models import Story
 from .serializers import StorySerializer
@@ -12,6 +13,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly # IsAuthenticat
 class StoryListView(APIView):
     permission_classes = (IsAuthenticatedOrReadOnly, ) # sets the permission levels of the specific view by passing in the rest framework authentication class
     # handle a GET request in the StoryListView
+    # Get all stories
     def get(self, _request):
         # go to the database and get all the stories
         stories = Story.objects.all()
@@ -20,6 +22,7 @@ class StoryListView(APIView):
         # return the serialized data and a 200 status code
         return Response(serialized_stories.data, status=status.HTTP_200_OK)
     
+    # Create a story
     def post(self, request):
         request.data["owner"] = request.user.id
         story_to_add = StorySerializer(data=request.data)
@@ -31,4 +34,44 @@ class StoryListView(APIView):
             print("Error")
             return Response(e.__dict__ if e.__dict__ else str(e), status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-   
+
+class StoryDetailView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly, ) # sets the permission levels of the specific view by passing in the rest framework authentication class
+
+    # Retrieving a single story from the database and error if it's not found
+    def get_story(self, pk):
+        try:
+            return Story.objects.get(pk=pk)
+        except Story.DoesNotExist:
+            raise NotFound(detail="Can't find that story!") # <-- imported the NotFound exception from rest_framwork.exceptions above
+
+    def get(self, _request, pk):
+        try:
+            story = Story.objects.get(pk=pk)
+            serialized_story = StorySerializer(story)
+            return Response(serialized_story.data, status=status.HTTP_200_OK)
+        except Story.DoesNotExist:
+            raise NotFound(detail="Can't find that story!") # <-- import the NotFound exception from rest_framwork.exceptions
+
+    def put(self, request, pk):
+        story_to_update = self.get_story(pk=pk)
+        if story_to_update.owner != request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        updated_story = StorySerializer(story_to_update, data=request.data)
+        if updated_story.is_valid():
+            updated_story.save()
+            return Response(updated_story.data, status=status.HTTP_202_ACCEPTED)
+
+        return Response(updated_story.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def delete(self, request, pk):
+        story_to_delete = self.get_story(pk=pk)
+
+        if story_to_delete.owner != request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        story_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)   
+    
+    
