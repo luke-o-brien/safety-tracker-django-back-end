17d2b37afa6d232a36ca696387391cacd7aae502
SylviaRemington <sylviaremington@gmail.com>
2025-10-16 12:51:52 -0700

    addtlnotes for author model

--- authors/models.py
+++ authors/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Model created here for author
 class Author(models.Model):
     def __str__(self):
         return f'{self.name}'
 
     name = models.CharField(max_length=80, unique=True)
 
-    
+
