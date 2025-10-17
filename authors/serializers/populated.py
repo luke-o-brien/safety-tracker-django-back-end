98a3015b10b0a1f2215e5131301f3c7b87a38486
SylviaRemington <sylviaremington@gmail.com>
2025-10-17 15:14:12 -0700

    PopulatedAuthorSerializer tested in Postman & everything is working

--- authors/serializers/populated.py
+++ authors/serializers/populated.py
@@ -4,3 +4,4 @@ from stories.serializers.common import StorySerializer
 class PopulatedAuthorSerializer(AuthorSerializer):
     stories = StorySerializer(many=True)
 
+# PopulatedAuthorSerializer tested in Postman & everything is working.
