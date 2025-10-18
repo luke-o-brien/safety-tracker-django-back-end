ef4f557b83319e0e71054ec782cbfd8011dda534
SylviaRemington <sylviaremington@gmail.com>
2025-10-18 01:30:33 -0700

    adding corsheaders middleware

--- project_safety_tracker/settings.py
+++ project_safety_tracker/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'project_safety_tracker.urls'
