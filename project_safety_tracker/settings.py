2678104bc18a14a99cae7bf6d085fe0ccc309054
SylviaRemington <sylviaremington@gmail.com>
2025-10-18 01:28:57 -0700

    moving corsheaders for common best Django practices

--- project_safety_tracker/settings.py
+++ project_safety_tracker/settings.py
@@ -37,12 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'authentication',
     'stories',
     'authors',
     'comments',
-    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'authentication.User'
