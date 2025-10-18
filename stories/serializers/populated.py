a6d039dbbe13a6bc863733e3b4d173d89e8ed00a
SylviaRemington <sylviaremington@gmail.com>
2025-10-18 00:19:41 -0700

    adding populating comments on stories via stories serializers populated.py

--- stories/serializers/populated.py
+++ stories/serializers/populated.py
@@ -1,8 +1,10 @@
 from .common import StorySerializer
 from authors.serializers.common import AuthorSerializer
 from authentication.serializers import UserSerializer
+from comments.serializers.populated import PopulatedCommentSerializer
 
 class PopulatedStorySerializer(StorySerializer):
     author = AuthorSerializer()
     owner = UserSerializer()
+    comments = PopulatedCommentSerializer(many=True)
     
