e9cb5e8c6e5919a894a0775bdf88a6d2bffb667b
SylviaRemington <sylviaremington@gmail.com>
2025-10-19 17:43:17 -0700

    check_ins - adding path to detail view

--- check_ins/urls.py
+++ check_ins/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Check_InListView
+from .views import Check_InListView, Check_InDetailView
 
 urlpatterns = [
   path('', Check_InListView.as_view()),
+  path('<int:pk>/', Check_InDetailView.as_view()),
 ]
