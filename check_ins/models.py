7d6b7186b085f63069edae3472104b02a3c83c91
SylviaRemington <sylviaremington@gmail.com>
2025-10-19 20:49:37 -0700

    deleting unnecessary code from check_ins models.py

--- check_ins/models.py
+++ check_ins/models.py
@@ -15,22 +15,4 @@ class Check_In(models.Model):
     related_name="check_ins",
     on_delete=models.CASCADE
 )
-
-# Alternate Check_In Model I could use as per researching (for future when I want to make it more accurate; however, maybe not use blank=True):
-# Create my Check_In Model
-# class Check_In(models.Model):
-#   def __str__(self):
-#     return f'{self.description} - {self.category}'
-#   description = models.CharField(max_length=5000)
-#   category = models.CharField(max_length=60, choices=[
-#       ('fear', 'Fear/Stress'),
-#       ('emotional', 'Emotional Abuse'),
-#       ('physical', 'Physical Abuse'),
-#       ('normal', 'Normal/Safe Day')
-#   ])
-#   date = models.DateField(auto_now_add=True)
-#   trigger_event = models.CharField(max_length=200, blank=True)
-#   reactionlevel = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)], blank=True)
-#   copingaction = models.CharField(max_length=60, blank=True)
-#   effectiveness = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)], blank=True)
-
+  
