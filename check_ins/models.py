b728583be9c4b2c810837d2d5acbca253e6f9a39
SylviaRemington <sylviaremington@gmail.com>
2025-10-25 23:20:56 -0700

    updating check_ins model

--- check_ins/models.py
+++ check_ins/models.py
@@ -2,17 +2,19 @@ from django.db import models
 
 # Create your models here - Creating my Check_In Model
 class Check_In(models.Model):
-  def __str__(self):
-    return f'{self.title} - {self.category}'
-  title = models.CharField(max_length=80, unique=True)
-  description = models.CharField(max_length=5000)
-  category = models.CharField(max_length=80)
-  reaction_level = models.FloatField()
-  coping_action = models.CharField(max_length=5000)
-  effectiveness = models.FloatField()
-  owner = models.ForeignKey(
-    "authentication.User",
-    related_name="check_ins",
-    on_delete=models.CASCADE
-)
-  
+    def __str__(self):
+        return f'{self.title} - {self.category}'
+    
+    title = models.CharField(max_length=80, unique=True)
+    description = models.CharField(max_length=5000)
+    day_type = models.CharField(max_length=100)  
+    relaxed_today = models.CharField(max_length=100) 
+    category = models.CharField(max_length=80)
+    reaction_level = models.FloatField()
+    coping_action = models.CharField(max_length=5000)
+    effectiveness = models.FloatField()
+    owner = models.ForeignKey(
+        "authentication.User",
+        related_name="check_ins",
+        on_delete=models.CASCADE
+    )
