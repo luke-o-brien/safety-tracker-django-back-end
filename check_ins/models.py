f765129566dd1d4b6a3bcc1a9f403f78e6c29d3e
SylviaRemington <sylviaremington@gmail.com>
2025-10-25 23:27:40 -0700

    correcting & updating check_ins models.py

--- check_ins/models.py
+++ check_ins/models.py
@@ -7,8 +7,8 @@ class Check_In(models.Model):
     
     title = models.CharField(max_length=80, unique=True)
     description = models.CharField(max_length=5000)
-    day_type = models.CharField(max_length=100)  
-    relaxed_today = models.CharField(max_length=100) 
+    day_type = models.CharField(max_length=100, default="Not specified")  
+    relaxed_today = models.CharField(max_length=100, default="Not specified") 
     category = models.CharField(max_length=80)
     reaction_level = models.FloatField()
     coping_action = models.CharField(max_length=5000)
